4b1a5d1aae149c4d57cf0ef6cad41c06e9d1c02f
Vladimir <scobca18@yandex.ru>
2024-12-08 02:14:58 +0300

    Test connection and creating the tables for mysql

---
+++ src/test_connection_mysql.py
@@ -0,0 +1,70 @@
+from idlelib import query
+
+import mysql.connector
+from mysql.connector import Error
+
+
+def create_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+
+
+def create_db(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
+connection = create_connection('localhost', 'root', '', 'test')
+
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
+create_users_table = """
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT, 
+  name TEXT NOT NULL, 
+  age INT, 
+  gender TEXT, 
+  nationality TEXT, 
+  PRIMARY KEY (id)
+) ENGINE = InnoDB
+"""
+
+execute_query(connection, create_users_table)
+
+create_posts_table = """
+CREATE TABLE IF NOT EXISTS posts (
+  id INT AUTO_INCREMENT, 
+  title TEXT NOT NULL, 
+  description TEXT NOT NULL, 
+  user_id INTEGER NOT NULL, 
+  FOREIGN KEY fk_user_id (user_id) REFERENCES users(id), 
+  PRIMARY KEY (id)
+) ENGINE = InnoDB
+"""
+
+execute_query(connection, create_posts_table)
